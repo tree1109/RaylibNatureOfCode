278abc03ad07bd6e1456c31bf6cd3ccc821d292f
tree1109 <55215480+tree1109@users.noreply.github.com>
2025-03-23 11:10:27 +0800

    modify playground 8

--- Playground8.cpp
+++ Playground8.cpp
@@ -96,12 +96,19 @@ int32_t main()
         oscillators.emplace_back(Vector2{ static_cast<float>(i), center.y });
 
         oscillators.back().SetAngularVelocity({ 2.0f,  5.0f });
-        oscillators.back().SetAmplitude({ 10.0f, static_cast<float>(windowWidth - i) * 0.1f});
+        oscillators.back().SetAmplitude({ 33.0f, static_cast<float>(windowWidth - i) * 0.1f});
         oscillators.back().SetColor(BLUE, LIGHTGRAY);
         oscillators.back().SetAngle({ i * 0.01f + PI * 0.5f, i * 0.01f });
     }
 
-    std::vector<Oscillator> oscillators2;
+    // Another wave.
+    float startAngle = 0.0f;
+    float startAngle2 = 35.0f;
+    constexpr float angleDelta = (2.0f * PI / windowWidth) * 5.0f;
+    constexpr float angularVelocity = angleDelta;
+    constexpr float angleDelta2 = (2.0f * PI / windowWidth);
+    constexpr float angularVelocity2 = angleDelta2 * 0.333f;
+    std::vector<Vector2> wavePoints;
 
     while (!WindowShouldClose())
     {
@@ -115,9 +122,41 @@ int32_t main()
                 oscillator.Update();
             }
 
-            for (int32_t i = 0; i < oscillators2.size(); ++i)
+            // Another wave.
             {
-                oscillators2[i].Update();
+                wavePoints.clear();
+
+                // wave1.
+                std::vector<float> waveValues;
+                float angle = startAngle;
+                for (int32_t x = 0; x < windowWidth; ++x)
+                {
+                    const float value = std::sin(angle) + std::sin(angle * 2.0f) * 0.5f + std::sin(angle * 5.0f) * 0.2f;
+                    waveValues.emplace_back(value);
+
+                    angle += angleDelta;
+                }
+                startAngle += angularVelocity;
+
+                // wave2.
+                std::vector<float> waveValues2;
+                float angle2 = startAngle2;
+                for (int32_t x = 0; x < windowWidth; ++x)
+                {
+                    const float value = std::sin(angle2 * 1.14514f);
+                    waveValues2.emplace_back(value);
+
+                    angle2 += angleDelta;
+                }
+                startAngle2 += angularVelocity2;
+
+                // combine wave.
+                for (int32_t x = 0; x < windowWidth; ++x)
+                {
+                    const float y = center.y * 1.5f + (waveValues[x] * 20.0f + waveValues2[x] * 15.0f);
+                    Vector2 point = { static_cast<float>(x), y };
+                    wavePoints.emplace_back(point);
+                }
             }
         }
 
@@ -129,9 +168,13 @@ int32_t main()
             {
                 oscillator.Draw();
             }
-            for (const auto& oscillator : oscillators2)
+
+            // Another wave.
+            DrawLineStrip(wavePoints.data(), static_cast<int>(wavePoints.size()), SKYBLUE);
+            for (const auto& point : wavePoints)
             {
-                oscillator.Draw();
+                if (std::fmod(point.x, 10.0f) != 0.0f) continue;
+                DrawLineV(point + Vector2{0.0f, 10.0f}, { point.x, static_cast<float>(windowHeight) }, BLUE);
             }
         }
         EndTextureMode();
