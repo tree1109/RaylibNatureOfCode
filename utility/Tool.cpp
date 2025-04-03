17887b0161e02724972eca018d3fd2f12ae5bb57
tree1109 <55215480+tree1109@users.noreply.github.com>
2025-04-03 13:42:29 +0800

    modify tool

--- utility/Tool.cpp
+++ utility/Tool.cpp
@@ -30,8 +30,17 @@ void tool::DrawReferenceCoordinate(const Vector2& origin, const float time)
     DrawLineEx(origin, angleArrowPos, thickness, BLUE);
     DrawCircleLinesV(origin, size, DARKBLUE);
 
-    DrawText("+X", static_cast<int>(xAxisArrowPos.x), static_cast<int>(xAxisArrowPos.y), 20, RED);
-    DrawText("+Y", static_cast<int>(yAxisArrowPos.x), static_cast<int>(yAxisArrowPos.y), 20, LIME);
-    DrawText("+Angle", static_cast<int>(angleArrowPos.x), static_cast<int>(angleArrowPos.y), 20, BLUE);
-    DrawText(TextFormat("%.0f, %.0f", origin.x, origin.y), static_cast<int>(origin.x), static_cast<int>(origin.y), 10, BLACK);
+    constexpr int fontSize = 10;
+
+    DrawText("+X", static_cast<int>(xAxisArrowPos.x), static_cast<int>(xAxisArrowPos.y), fontSize, RED);
+    DrawText("+Y", static_cast<int>(yAxisArrowPos.x), static_cast<int>(yAxisArrowPos.y), fontSize, LIME);
+    DrawText("+Angle", static_cast<int>(angleArrowPos.x), static_cast<int>(angleArrowPos.y), fontSize, BLUE);
+
+    const int infoX = static_cast<int>(origin.x) + fontSize;
+    const int infoY = static_cast<int>(origin.y) + fontSize;
+    const int degrees = static_cast<int>(RAD2DEG * time) % 360;
+
+    DrawText(TextFormat("X: %d", infoX), infoX, infoY, fontSize, RED);
+    DrawText(TextFormat("Y: %d", infoY), infoX, (infoY + fontSize), fontSize, LIME);
+    DrawText(TextFormat("Deg: %d", degrees), infoX, (infoY + fontSize * 2), fontSize, BLUE);
 }
